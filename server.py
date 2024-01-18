c05976ec73807b4109ca09e509eaab82d2bb64cb
kinyarasam <skinyara.30@gmail.com>
2024-01-18 15:32:17 +0300

    Flask server

---
+++ server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from os import environ
+from api.v1.app import app
+
+if __name__ == "__main__":
+    """ Main Function
+    """
+    host = environ.get('SACCO_API_HOST')
+    port = environ.get('SACCO_API_PORT')
+
+    host = '0.0.0.0' if not host else host
+    port = '5000' if not port else port
+    app.run(host=host, port=port, threaded=True)
